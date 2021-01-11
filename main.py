acb53d4db576c98a043f86628da8c891dd605656
TheAngryPopo <63618212+TheAngryPopo@users.noreply.github.com>
2021-01-11 10:49:36 -0500

    testing

--- main.py
+++ main.py
@@ -7,9 +7,9 @@
 import pygame
 
 #Import Other Files
-from level_design.py import *
-from global_variables.py import *
-from player_data.py import *
+#from level_design.py import *
+#from global_variables.py import *
+#from player_data.py import *
 
 pygame.init()
 
@@ -19,17 +19,85 @@ global current_level
 # Set up the drawing window
 screen = pygame.display.set_mode([800, 600])
 
-# Run until the user asks to quit
-while True:
+m,v = 1,5
 
-    # Did the user click the window close button?
+def jump(player_number):
+    global m,v
+    
+    while True:
+        # calculate force (F). F = 1 / 2 * mass * velocity ^ 2. 
+        F = ((1 / 2)*m*(v**2))*2
+            
+        # change in the y co-ordinate 
+        if player_number == 0:
+            Player0.y -= F
+            
+        # decreasing velocity while going up and become negative while coming down 
+        v = v-1
+            
+        # object reached its maximum height 
+        if v<0: 
+            # negative sign is added to counter negative velocity 
+            m =-1
+
+        # objected reaches its original state
+        if v == -6:
+            m,v = 1,5
+            main(False)
+        else:
+            main(True)
+
+
+def check_events():
+    #check all events that happened in this frame
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            break
+        #pressed = pygame.key.get_pressed()
+        #if pressed[pygame.K_w]:
+        #    Player0.y = Player0.y + 1
 
-    display_level(testing_level,screen)
-    pygame.draw.rect(screen, (0, 0, 255), (player1_attributes))
-    pygame.display.flip()
+        # Is there a key pressed down?
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w: #if pressing w
+                #make the player jump
+                jump(0)
+        
+        # Did the user click the window close button?
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+
+class Sprite:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+Player0 = Sprite(100, 100, 100, 100)
+
+Players = [Player0]
+
+def main(jumping):
+    # Run until the user asks to quit
+    while True:
+
+        screen.fill((0,0,0))
+
+        #check for any events this frame
+        check_events()
+
+        #display_level(testing_level,screen)
+
+        if jumping:
+            jump(0)
+
+        #print(Player0.y)
+        pygame.draw.rect(screen, (0, 0, 255), (Player0.x,Player0.y,Player0.w,Player0.h))
+        pygame.display.flip()
+        
+
+#start the program
+main(False)
 
 # Done! Time to quit.
 pygame.quit()
